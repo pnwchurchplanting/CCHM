73ffcd6d8039e1f5fe9d1101671dd00877e1b879
Josh Fry <joshdfry@me.com>
2024-08-05 18:00:54 -0600

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,23 +12,20 @@ const IndexPage: React.FC<PageProps> = () => {
         edges {
           node {
             title
-            splashImage {
-              url
-            }
           }
         }
       }
     }
   `)
 
-  const { title, splashImage } = data.allContentfulTest.edges[0].node
+  const { title } = data.allContentfulTest.edges[0].node
 
   return (
     <Layout>
       <div
         className="splash"
         style={{
-          backgroundImage: `url(${splashImage.gatsbyImageData.images.fallback.src})`,
+          backgroundImage: `url(https://images.ctfassets.net/thw99qcq417p/4w5blxepB6UIEVbWzrqAaC/e6647b4b38a980e80be5fe1ea75d05e5/pnw-bg.jpeg?w=3000&h=2000&fl=progressive&q=50&fm=jpg)`,
         }}
       >
         <Section>
